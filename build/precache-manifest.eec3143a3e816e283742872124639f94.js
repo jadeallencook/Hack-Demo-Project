7fb83d7ff18b48cca323f6189f237581823278a7
Jade Allen Cook <jadeallencook@gmail.com>
2020-03-24 20:14:47 -0700

    Exported production build

---
+++ build/precache-manifest.eec3143a3e816e283742872124639f94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1150976b514df458d249e31bd8ecaaa1",
+    "url": "/Hack-Demo-Project/build/index.html"
+  },
+  {
+    "revision": "537084661bcf510c0fc6",
+    "url": "/Hack-Demo-Project/build/static/css/main.3621d7f7.chunk.css"
+  },
+  {
+    "revision": "f64e97e6d8c03ba84ac1",
+    "url": "/Hack-Demo-Project/build/static/js/2.f876df4f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Hack-Demo-Project/build/static/js/2.f876df4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "537084661bcf510c0fc6",
+    "url": "/Hack-Demo-Project/build/static/js/main.1be19ac2.chunk.js"
+  },
+  {
+    "revision": "4002b4896386fcf6da92",
+    "url": "/Hack-Demo-Project/build/static/js/runtime-main.64683e3e.js"
+  }
+]);
